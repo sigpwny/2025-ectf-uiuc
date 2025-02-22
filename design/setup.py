b5397e2521fe9fe03cb37a3a0098b8dcd742b049
Minh Duong <git@whitehoodhacker.net>
2025-02-22 13:44:45 -0600

    feat: pip installable wrapper

--- design/setup.py
+++ design/setup.py
@@ -1,41 +1,21 @@
 from setuptools import setup
-from setuptools.command.install import install
 import sys
 import subprocess
 import os
-import glob
 
-class InstallWithWheel(install):
-    def run(self):
-        # Get the Python version and platform
-        py_version = f"cp{sys.version_info.major}{sys.version_info.minor}"
-        platform = sys.platform
-        arch = "win_amd64" if platform == "win32" else \
-               "manylinux_x86_64" if "linux" in platform else \
-               "macosx_10_13_x86_64"
+# Installs the ectf25_design prebuilt wheel
+def install_wheel():
+    wheels_dir = os.path.join(os.path.dirname(__file__), "wheels")
+    subprocess.check_call([
+        sys.executable, "-m", "pip", "install", "--no-index", "--force-reinstall",
+        "--find-links", wheels_dir, "ectf25_design"
+    ])
 
-        # Find the matching wheel
-        wheels_dir = os.path.join(os.path.dirname(__file__), "wheels")
-        matching_wheel = None
-        # wheel files could either be .whl or .zip or .tar.gz
-        wheel_files = glob.glob(os.path.join(wheels_dir, "*.whl")) + glob.glob(os.path.join(wheels_dir, "*.zip")) + glob.glob(os.path.join(wheels_dir, "*.tar.gz"))
-        for wheel in wheel_files:
-            if py_version in wheel and arch in wheel:
-                matching_wheel = wheel
-                break
-
-        if matching_wheel:
-            print(f"Installing {matching_wheel}")
-            subprocess.check_call([sys.executable, "-m", "pip", "install", matching_wheel])
-        else:
-            raise RuntimeError("No compatible wheel found for this platform.")
-
-        install.run(self)
+install_wheel()
 
 setup(
-    name="ectf25_design",
+    name="ectf25_design_wrapper",
     version="1.0.0+uiuc",
-    packages=["ectf25_design"],
-    cmdclass={"install": InstallWithWheel},
+    packages=["wrapper"],
     include_package_data=True,
 )
