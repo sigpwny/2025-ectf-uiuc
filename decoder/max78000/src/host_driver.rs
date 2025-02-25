33e4def7821cfc03c9c57669847088fc71a0f098
Minh Duong <git@whitehoodhacker.net>
2025-02-25 03:26:16 -0600

    feat(common, decoder): add bincode and WIP decoding (#45)
    * Frame Decode Near Done, Needs Nonce
    
    * Partially Done Decode Frames
    
    * feat: DRAFT: bincode support in common
    
    * rename frame.rs to decode.rs
    
    * feat: message types with bincode
    
    * test: add other direction to test
    
    * feat: add better error message for wrong length
    
    * feat: bincode for host UART receive
    
    Co-authored-by: henopied <henopied@users.noreply.github.com>
    
    * fix: corrected invalid length error type
    
    * refactor(common): constant fixes and naming
    
    ---------
    
    Co-authored-by: yyaasshh1018 <coolyg1018@gmail.com>
    Co-authored-by: henopied <henopied@users.noreply.github.com>

--- decoder/max78000/src/host_driver.rs
+++ decoder/max78000/src/host_driver.rs
@@ -1,3 +1,11 @@
+use bincode::{
+    de::{Decode, read::Reader},
+    enc::{Encoder, write::Writer},
+    error::DecodeError,
+    decode_from_reader,
+};
+use common::{MessageToDecoder, MessageFromDecoder, BINCODE_CONFIG};
+use common::constants::*;
 use core::convert::Infallible;
 use embedded_hal_nb::nb::block;
 use embedded_hal_nb::serial;
@@ -18,11 +26,15 @@ pub enum MessageType {
     Debug,
 }
 
-#[derive(Eq, PartialEq, Clone)]
-pub enum ErrorHeader {
-    UartError,
+pub enum UartError {
+    Decode(DecodeError),
     InvalidOpcode,
     InvalidLength,
+}
+
+pub enum UartState {
+    None,
+    NumBytesRead(usize)
 }
 
 pub struct MessageHeader {
@@ -95,6 +107,18 @@ impl Message {
             data,
         }
     }
+
+    pub fn decode(message: &[u8]) -> Self {
+        let mut data = [0u8; MAX_MESSAGE_SIZE];
+        data[..message.len()].copy_from_slice(message);
+        Self {
+            header: MessageHeader {
+                opcode: MessageType::Decode,
+                length: message.len() as u16,
+            },
+            data,
+        }
+    }
 }
 
 /// A driver for the host computer and decoder interface as described in the
@@ -104,6 +128,28 @@ where
     Serial: serial::Read<u8, Error = SerialError> + serial::Write<u8, Error = SerialError>,
 {
     uart: Serial,
+    state: UartState,
+}
+
+impl<Serial, SerialError> Reader for HostDriver<Serial, SerialError>
+where
+    Serial: serial::Read<u8, Error = SerialError> + serial::Write<u8, Error = SerialError>,
+{
+    fn read(&mut self, buf: &mut [u8]) -> Result<(), DecodeError> {
+        for b in buf.iter_mut() {
+            self.state = match self.state {
+                UartState::NumBytesRead(0) => {
+                    self.write_ack();
+                    UartState::NumBytesRead(1)
+                }
+                UartState::NumBytesRead(n @ 0..255) => UartState::NumBytesRead(n + 1),
+                UartState::NumBytesRead(255) => UartState::NumBytesRead(0),
+                _ => unreachable!("Invalid state"),
+            };
+            *b = block!(self.uart.read()).map_err(|_| DecodeError::Other("UART read error"))?
+        }
+        Ok(())
+    }
 }
 
 impl<Serial, SerialError> HostDriver<Serial, SerialError>
@@ -112,7 +158,7 @@ where
 {
     /// Create a new host transport driver with the given serial interface.
     pub fn new(uart: Serial) -> Self {
-        Self { uart }
+        Self { uart, state: UartState::None }
     }
 
     /// Free the serial interface from the host transport driver.
@@ -121,46 +167,64 @@ where
     }
 
     /// Read a message from the host computer.
-    pub fn read_message(&mut self) -> Message {
-        let mut message = Message::new();
+    pub fn read_message(&mut self) -> Result<MessageToDecoder, UartError> {
+        let header = self.read_header()?;
+
+        self.state = UartState::NumBytesRead(0);
+
+        let result = match (header.opcode, header.length as usize) {
+            (MessageType::List, 0) => Ok(MessageToDecoder::ListSubscriptions),
+            (MessageType::Subscribe, LEN_ENCRYPTED_SUBSCRIPTION) => Ok(MessageToDecoder::UpdateSubscription(
+                decode_from_reader(&mut *self, BINCODE_CONFIG).map_err(|e| UartError::Decode(e))?,
+            )),
+            (MessageType::Decode, LEN_ENCRYPTED_FRAME) => Ok(MessageToDecoder::DecodeFrame(decode_from_reader(&mut *self, BINCODE_CONFIG).map_err(|e| UartError::Decode(e))?)),
+            (MessageType::List|MessageType::Subscribe|MessageType::Decode, _) => Err(UartError::InvalidLength),
+            _ => Err(UartError::InvalidOpcode),
+        };
+
+        self.write_ack();
+
+        result
+
+        // let mut message = Message::new();
 
         // TODO: Add random delay here
 
         // Keep reading headers until we get a valid one
-        loop {
-            let res = self.read_header();
-            self.write_ack();
-            match res {
-                Ok(header) => {
-                    message.header = header;
-                    break;
-                }
-                Err(_) => {
-                    self.write_message(Message::error());
-                    self.read_ack();
-                }
-            }
-        }
+        // loop {
+        //     let res = self.read_header();
+        //     self.write_ack();
+        //     match res {
+        //         Ok(header) => {
+        //             message.header = header;
+        //             break;
+        //         }
+        //         Err(_) => {
+        //             self.write_message(Message::error());
+        //             self.read_ack();
+        //         }
+        //     }
+        // }
 
-        // Read the message data in blocks of BLOCK_SIZE
-        // After each block, send an ACK message
-        // Read until either message.data is full or the header length is reached
-        let mut bytes_read = 0;
-        let read_limit = core::cmp::min(message.header.length as usize, message.data.len());
-        while bytes_read < read_limit {
-            let end = core::cmp::min(bytes_read + BLOCK_SIZE, read_limit);
-            for i in bytes_read..end {
-                if let Ok(val) = block!(self.uart.read()) {
-                    message.data[i] = val;
-                    bytes_read += 1;
-                }
-            }
-            self.write_ack();
-        }
+        // // Read the message data in blocks of BLOCK_SIZE
+        // // After each block, send an ACK message
+        // // Read until either message.data is full or the header length is reached
+        // let mut bytes_read = 0;
+        // let read_limit = core::cmp::min(message.header.length as usize, message.data.len());
+        // while bytes_read < read_limit {
+        //     let end = core::cmp::min(bytes_read + BLOCK_SIZE, read_limit);
+        //     for i in bytes_read..end {
+        //         if let Ok(val) = block!(self.uart.read()) {
+        //             message.data[i] = val;
+        //             bytes_read += 1;
+        //         }
+        //     }
+        //     self.write_ack();
+        // }
 
-        // TODO: Add random delay here
+        // // TODO: Add random delay here
 
-        message
+        // message
     }
 
     /// Write a message to the host computer.
@@ -204,7 +268,7 @@ where
     }
 
     /// Helper function to read a header from the host computer.
-    fn read_header(&mut self) -> Result<MessageHeader, ErrorHeader> {
+    fn read_header(&mut self) -> Result<MessageHeader, UartError> {
         let mut header = MessageHeader::new();
         let mut magic_value = 0u8;
 
@@ -212,7 +276,7 @@ where
         while magic_value != b'%' {
             match block!(self.uart.read()) {
                 Ok(val) => magic_value = val,
-                Err(_) => return Err(ErrorHeader::UartError),
+                Err(_) => return Err(UartError::Decode(DecodeError::Other("UART read error"))),
             }
         }
 
@@ -226,10 +290,10 @@ where
                     b'A' => MessageType::Ack,
                     b'E' => MessageType::Error,
                     b'G' => MessageType::Debug,
-                    _ => return Err(ErrorHeader::InvalidOpcode),
+                    _ => return Err(UartError::InvalidOpcode),
                 }
             }
-            Err(_) => return Err(ErrorHeader::UartError),
+            Err(_) => return Err(UartError::Decode(DecodeError::Other("UART read error"))),
         };
 
         // Length
@@ -237,13 +301,10 @@ where
         for b in length_bytes.iter_mut() {
             match block!(self.uart.read()) {
                 Ok(val) => *b = val,
-                Err(_) => return Err(ErrorHeader::UartError),
+                Err(_) => return Err(UartError::Decode(DecodeError::Other("UART read error"))),
             }
         }
         header.length = u16::from_le_bytes(length_bytes);
-        if header.length > MAX_MESSAGE_SIZE as u16 {
-            return Err(ErrorHeader::InvalidLength);
-        }
 
         Ok(header)
     }
